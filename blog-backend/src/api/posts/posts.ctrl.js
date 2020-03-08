6faa9a491650776dcd4d3b3cb45db28c05747f45
billowycloud <54979977+billowycloud@users.noreply.github.com>
2020-03-08 16:24:47 +0900

    프론트엔드 글쓰기 & 포스트 조회 구현

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,8 +1,33 @@
 import Post from "../../models/post";
 import mongoose from "mongoose";
-import Joi from "joi";
+import Joi from "@hapi/joi";
+import sanitizeHtml from "sanitize-html";
 
 const { ObjectId } = mongoose.Types;
+
+const sanitizeOption = {
+  allowedTags: [
+    "h1",
+    "h2",
+    "b",
+    "i",
+    "u",
+    "s",
+    "p",
+    "ul",
+    "ol",
+    "li",
+    "blockquote",
+    "a",
+    "img"
+  ],
+  allowedAttributes: {
+    a: ["href", "name", "target"],
+    img: ["src"],
+    li: ["class"]
+  },
+  allowedSchemes: ["data", "http"]
+};
 
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
@@ -53,7 +78,7 @@ export const write = async ctx => {
   });
 
   // 검증 후, 검증 실패시 에러처리
-  const result = Joi.validate(ctx.request.body, schema);
+  const result = schema.validate(ctx.request.body);
   if (result.error) {
     ctx.status = 400; // Bad Request
     ctx.body = result.error;
@@ -63,7 +88,7 @@ export const write = async ctx => {
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
-    body,
+    body: sanitizeHtml(body, sanitizeOption),
     tags,
     user: ctx.state.user
   });
@@ -73,6 +98,13 @@ export const write = async ctx => {
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+const removeHtmlAndShorten = body => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: []
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
 };
 
 /*
@@ -107,8 +139,7 @@ export const list = async ctx => {
       //.map(post => post.toJSON()) - lean()함수를 통해 생략
       .map(post => ({
         ...post,
-        body:
-          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...` // 200자 이상이면 ...[내용 길이 제한]
+        body: removeHtmlAndShorten(post.body)
       }));
   } catch (e) {
     ctx.throw(500, e);
@@ -153,15 +184,21 @@ export const update = async ctx => {
   });
 
   // 검증 후, 검증 실패시 에러처리
-  const result = Joi.validate(ctx.request.body, schema);
+  const result = schema.validate(ctx.request.body);
   if (result.error) {
     ctx.status = 400; // Bad Request
     ctx.body = result.error;
     return;
   }
 
+  const nextData = { ...ctx.request.body }; //객체를 복사하고
+  //body 값이 주어졌으면 HTML 필터링
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body);
+  }
+
   try {
-    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
     }).exec();
