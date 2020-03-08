b3db0b4dd6a5592d85f03e32fccf83cb13c3801b
billowycloud <54979977+billowycloud@users.noreply.github.com>
2020-03-08 16:38:10 +0900

    수정

--- blog-frontend/src/containers/posts/PaginationContainer.js
+++ blog-frontend/src/containers/posts/PaginationContainer.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { withRouter } from "react-router-dom";
 import qs from "qs";
 
-const PaginationContainer = ({ location }) => {
+const PaginationContainer = ({ location, match }) => {
   const { lastPage, posts, loading } = useSelector(({ posts, loading }) => ({
     lastPage: posts.lastPage,
     posts: posts.posts,
@@ -15,7 +15,8 @@ const PaginationContainer = ({ location }) => {
   if (!posts || loading) return null;
 
   // page가 없으면 1을 기본값으로 사용
-  const { tag, username, page = 1 } = qs.parse(location.search, {
+  const { username } = match.params;
+  const { tag, page = 1 } = qs.parse(location.search, {
     ignoreQueryPrefix: true
   });
 
