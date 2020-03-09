0221eb39f9bc1bf8eeb222e69da199c10f62ab06
billowycloud <54979977+billowycloud@users.noreply.github.com>
2020-03-09 17:26:18 +0900

    리액트 헬멧 적용

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -9,6 +9,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer, { rootSaga } from "./modules";
 import createSagaMiddleware from "redux-saga";
 import { tempSetUser, check } from "./modules/user";
+import { HelmetProvider } from "react-helmet-async";
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -41,7 +42,9 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <GlobalStyles />
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
